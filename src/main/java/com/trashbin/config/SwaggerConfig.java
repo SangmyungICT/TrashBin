b613a1b8506f3de67aa61cba9483e852703f251b
gujaeseong <gjs5448@gmail.com>
2023-06-01 14:29:17 +0900

    swagger 3로 변경

--- src/main/java/com/trashbin/config/SwaggerConfig.java
+++ src/main/java/com/trashbin/config/SwaggerConfig.java
@@ -8,16 +8,15 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
-public class SwaggerConfiguration {
+public class SwaggerConfig {
 
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
+    public Docket api(){
+        return new Docket(DocumentationType.OAS_30)
                 .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.trashbin"))
                 .paths(PathSelectors.any())
@@ -31,4 +30,5 @@ public class SwaggerConfiguration {
                 .version("1.0")
                 .build();
     }
+
 }
